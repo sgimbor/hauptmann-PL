b7415cd55846bfcedd99eb6524b8ba8870f6eb4c
sgimbor <s.gimbor@gmail.com>
2025-09-06 15:07:53 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Test1");
+let scrollTarget = 0;
+let currentScroll = 0;
+
+window.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  scrollTarget += e.deltaY; // целевая позиция
+}, { passive: false });
+
+function animate() {
+  currentScroll += (scrollTarget - currentScroll) * 0.1; // "инерция"
+  window.scrollTo(0, currentScroll);
+  requestAnimationFrame(animate);
+}
+animate();
