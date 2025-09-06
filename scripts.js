42befdf4cf0bd6f6176c5aaae17e5b8e5f2c4267
sgimbor <s.gimbor@gmail.com>
2025-09-06 15:51:00 +0300

    1

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
-  if (!matchMedia('(prefers-reduced-motion: reduce)').matches) {
+document.addEventListener('click', e => {
+  const a = e.target.closest('a[href="#"]');
+  if (a) e.preventDefault(); // ничего не делаем для "#"
+});
+
+if (!matchMedia('(prefers-reduced-motion: reduce)').matches) {
     const lenis = new Lenis({
-      duration: 1.2,          // общая «вязкость» (1.0–1.6)
-      easing: (t) => 1 - Math.pow(1 - t, 4), // плавное затухание
-      smoothWheel: true,
-      wheelMultiplier: 1.0,   // чувствительность колеса
-      touchMultiplier: 1.2,   // тачпад/тач
-      smoothTouch: false
+        duration: 1.2, // общая «вязкость» (1.0–1.6)
+        easing: (t) => 1 - Math.pow(1 - t, 4), // плавное затухание
+        smoothWheel: true,
+        wheelMultiplier: 1.0, // чувствительность колеса
+        touchMultiplier: 1.2, // тачпад/тач
+        smoothTouch: false
     });
 
     function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
+        lenis.raf(time);
+        requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
-  }
+}
